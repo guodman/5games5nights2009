7943cafdb277828567f28c8c17f5fea9d8305ad3
doug <doug@85295b6a-e990-43dc-9b57-ec326446024b>
2009-07-31 20:54:02 +0000

    method for getting a random card, Move can draw itself
    git-svn-id: svn+ssh://guodman.org/usr/local/svn/incubator/trunk@27 85295b6a-e990-43dc-9b57-ec326446024b

--- Line/src/com/mortalpowers/games/line/Card.java
+++ Line/src/com/mortalpowers/games/line/Card.java
@@ -15,15 +15,20 @@ public abstract class Card extends Renderable {
 		cardText = text;
 	}
 	
+	public static Card getRandomCard() {
+		return new Move(3);
+	}
+	
 	public abstract void action();
 	public abstract void init(GameContainer container);
 	public abstract void update(GameContainer container, int delta);
 	
-	public class Move extends Card {
+	public static class Move extends Card {
 		private int distance = 3;
 		
-		public Move() {
+		public Move(int distance) {
 			super("Movement", "Move forward in line");
+			this.distance = distance;
 		}
 
 		@Override
@@ -34,16 +39,13 @@ public abstract class Card extends Renderable {
 
 		@Override
 		public void init(GameContainer container) {}
-
 		@Override
 		public void render(GameContainer container, Graphics g) {
 			g.setColor(Color.cyan);
+			g.drawString(cardText, x, y);
+			g.drawString("Distance: " + distance, x, y+15);
 		}
-
 		@Override
-		public void update(GameContainer container, int delta) {
-			// TODO Auto-generated method stub
-			
-		}
+		public void update(GameContainer container, int delta) {}
 	}
 }
