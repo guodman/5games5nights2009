21d06b4f36064ce4a45ef8869b2fdfdad52c9949
Stephen Fluin <Stephen Fluin@85295b6a-e990-43dc-9b57-ec326446024b>
2009-08-03 23:40:21 +0000

    Added screen setup.
    git-svn-id: svn+ssh://guodman.org/usr/local/svn/incubator/trunk@52 85295b6a-e990-43dc-9b57-ec326446024b

--- Cow/src/org/guodman/cow/CowGame.java
+++ Cow/src/org/guodman/cow/CowGame.java
@@ -5,14 +5,18 @@ import java.util.List;
 
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 public class CowGame extends BasicGame {
 	public static final int WIDTH=1024;
 	public static final int HEIGHT=768;
-
+	final public static int tileSize = 64;
+public static boolean quit = false;
+	
 	/**
 	 * @param args
 	 */
@@ -51,14 +55,24 @@ public class CowGame extends BasicGame {
 	public void update(GameContainer container, int delta)
 			throws SlickException {
 		// TODO Auto-generated method stub
-		
+		if(quit) {
+			container.exit();
+		}
 	}
 
 	@Override
 	public void render(GameContainer container, Graphics g)
 			throws SlickException {
 		// TODO Auto-generated method stub
-		
+		g.setColor(Color.lightGray);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		g.setColor(Color.black);
+		for (int i = tileSize; i < WIDTH; i += tileSize) {
+			g.drawLine(i, 0, i, HEIGHT);
+		}
+		for (int i = tileSize; i < HEIGHT; i += tileSize) {
+			g.drawLine(0, i, WIDTH, i);
+		}
 	}
 
 	public void mouseMoved(final int oldx, final int oldy, final int newx,
@@ -76,5 +90,19 @@ public class CowGame extends BasicGame {
 
 	public void mouseWheelMoved(final int arg0) {
 		
+	}
+	public void keyPressed(int key, char c) {
+		// System.out.println("Someone pressed " + key);
+
+		switch (key) {
+
+
+		case Input.KEY_ESCAPE:
+			quit = true;
+			break;
+		
+
+		}
+
 	}
 }
