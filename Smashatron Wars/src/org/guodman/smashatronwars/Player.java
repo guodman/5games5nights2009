401f9b26b443948edd1c385b4d60592182c8a8b1
Stephen Fluin <Stephen Fluin@85295b6a-e990-43dc-9b57-ec326446024b>
2009-08-04 23:35:49 +0000

    Movement handling with controller.
    git-svn-id: svn+ssh://guodman.org/usr/local/svn/incubator/trunk@83 85295b6a-e990-43dc-9b57-ec326446024b

--- Smashatron Wars/src/org/guodman/smashatronwars/Player.java
+++ Smashatron Wars/src/org/guodman/smashatronwars/Player.java
@@ -6,14 +6,20 @@ import org.newdawn.slick.Graphics;
 
 public class Player {
 	float x, y;
+	int width, height;
+
+	
 	public Player() {
 		x = SmashatronWarsGame.WIDTH / 2 - 15;
 		y = SmashatronWarsGame.HEIGHT / 2 - 15;
+		
+		width = 30;
+		height = 30;
 	}
 	
 	public void render(GameContainer c, Graphics g) {
-		g.drawOval(x,y,30, 30);
+		g.drawOval(x,y,width, height);
 		g.setColor(Color.blue);
-		g.fillOval(x,y,30,30);
+		g.fillOval(x,y,width,height);
 	}
 }
--- Smashatron Wars/src/org/guodman/smashatronwars/SmashatronWarsGame.java
+++ Smashatron Wars/src/org/guodman/smashatronwars/SmashatronWarsGame.java
@@ -12,21 +12,23 @@ import org.newdawn.slick.SlickException;
 public class SmashatronWarsGame extends BasicGame {
 	public static final int WIDTH = 1024;
 	public static final int HEIGHT = 768;
+	public static final float SPEED = 5;
 	public static boolean quit;
+	public Player player;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		try {
-			AppGameContainer container = new AppGameContainer(new SmashatronWarsGame(),
-					WIDTH, HEIGHT, false);
+			AppGameContainer container = new AppGameContainer(
+					new SmashatronWarsGame(), WIDTH, HEIGHT, false);
 			container.start();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public Controller joystick = null;
 
 	public SmashatronWarsGame() {
@@ -40,33 +42,80 @@ public class SmashatronWarsGame extends BasicGame {
 			if (Controllers.getControllerCount() > 0) {
 				joystick = Controllers.getController(0);
 				System.out.println("Joystick Name: " + joystick.getName());
-				System.out.println("Joystick has "+joystick.getButtonCount() +" buttons. Its name is "+joystick.getName());
-			} else joystick = null;
+				System.out.println("Joystick has " + joystick.getButtonCount()
+						+ " buttons. Its name is " + joystick.getName());
+			} else
+				joystick = null;
+		} catch (org.lwjgl.LWJGLException e) {
+			System.err.println("Couldn't initialize Controllers: "
+					+ e.getMessage());
 		}
-		catch (org.lwjgl.LWJGLException e) {System.err.println("Couldn't initialize Controllers: "+e.getMessage());}
+		player = new Player();
 	}
 
 	@Override
 	public void update(GameContainer container, int delta)
 			throws SlickException {
-		if(quit) {
+		if (quit) {
 			container.exit();
 		}
+
+		if (joystick != null) {
+			float x1, y1, x2, y2;
+			switch (joystick.getAxisCount()) {
+			// Dougbert controller
+			case 4:
+				x1 = joystick.getAxisValue(0);
+				y1 = joystick.getAxisValue(1);
+				x2 = joystick.getAxisValue(2);
+				y2 = joystick.getAxisValue(3);
+				break;
+
+			// Xbox controller
+			case 7:
+				x1 = joystick.getAxisValue(1);
+				y1 = joystick.getAxisValue(2);
+				x2 = joystick.getAxisValue(4);
+				y2 = joystick.getAxisValue(5);
+
+				break;
+
+			default:
+				x1 = x2 = y1 = y2 = 0;
+			}
+			/**
+			 * Sanitize close to zero values.
+			 */
+			if(Math.abs(x1) < .1) x1 = 0;
+			if(Math.abs(x2) < .1) x2 = 0;
+			if(Math.abs(y1) < .1) y1 = 0;
+			if(Math.abs(y2) < .1) y2 = 0;
+			
+			
+			player.x += x1 * SPEED;
+			player.y += y1 * SPEED;
+			
+			/**
+			 * Sanitize board values
+			 */
+			if()
+		}
+
 	}
 
 	@Override
 	public void render(GameContainer container, Graphics g)
 			throws SlickException {
 		if (joystick != null) {
-			String joyInfo="Joystick Info: ";
-			for (int i=0; i < joystick.getAxisCount(); i++) {
+			String joyInfo = "Joystick Info: ";
+			for (int i = 0; i < joystick.getAxisCount(); i++) {
 				joyInfo += joystick.getAxisValue(i) + " : ";
 			}
 			g.drawString(joyInfo, 10, 25);
 		}
-		new Player().render(container,g);
+		player.render(container, g);
 	}
-	
+
 	public void keyPressed(int key, char c) {
 		// System.out.println("Someone pressed " + key);
 
