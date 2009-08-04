9ef31dac42859efbb951300db1e953f7895daa40
doug <doug@85295b6a-e990-43dc-9b57-ec326446024b>
2009-08-04 22:04:16 +0000

    fix
    git-svn-id: svn+ssh://guodman.org/usr/local/svn/incubator/trunk@76 85295b6a-e990-43dc-9b57-ec326446024b

---
+++ Smashatron Wars/src/org/guodman/smashatronwars/SmashatronWarsGame.java
@@ -0,0 +1,71 @@
+package org.guodman.smashatronwars;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class SmashatronWarsGame extends BasicGame {
+
+	public SmashatronWarsGame() {
+		super("Smashatron Wars");
+	}
+
+	@Override
+	public void init(GameContainer container) throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(GameContainer container, int delta)
+			throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void controllerButtonPressed(final int arg0, final int arg1) {
+	}
+
+	public void controllerButtonReleased(final int arg0, final int arg1) {
+	}
+
+	public void controllerDownPressed(final int arg0) {
+	}
+
+	public void controllerDownReleased(final int arg0) {
+	}
+
+	public void controllerLeftPressed(final int arg0) {
+	}
+
+	public void controllerLeftReleased(final int arg0) {
+	}
+
+	public void controllerRightPressed(final int arg0) {
+	}
+
+	public void controllerRightReleased(final int arg0) {
+	}
+
+	public void controllerUpPressed(final int arg0) {
+	}
+
+	public void controllerUpReleased(final int arg0) {
+	}
+}
